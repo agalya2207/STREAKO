4bdb2f485d0d34c03804aee92d74e1e3e775667e
agalya2207 <agalya.2207@gmail.com>
2026-08-30 22:32:12 +0530

    set login/signup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Streako Backend API is running!' });
+});
+
+// Auth routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
